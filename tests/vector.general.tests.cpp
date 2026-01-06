89854979d41c015e91c6c879f88076cea827ff20
Ricardo Baucells <ricardo@baucells.net>
2026-01-06 17:11:11 -0500

    Tests for norms

--- tests/vector.general.tests.cpp
+++ tests/vector.general.tests.cpp
@@ -132,3 +132,83 @@ TEST(VectorGeneral, conjugate_complex) {
     // assert
     ASSERT_TRUE(conj.equals(expected, 0.001f));
 }
+
+TEST(VectorGeneral, taxicab_norm_real) {
+    // arrange
+    constexpr Vector<3> a = {-1, 2, -3};
+    constexpr float expected = 6;
+    // act
+    const float norm = a.taxicabNorm();
+    // assert
+    ASSERT_TRUE(compare(norm, expected, 0.001f));
+}
+
+TEST(VectorGeneral, taxicab_norm_complex) {
+    // arrange
+    constexpr Vector<3, std::complex<float>> a = {{-1, 2}, {3, 4}, {5, -6}};
+    constexpr float expected = 15.04632f;
+    // act
+    const float norm = a.taxicabNorm();
+    // assert
+    ASSERT_TRUE(compare(norm, expected, 0.001f));
+}
+
+TEST(VectorGeneral, euclidian_norm_real) {
+    // arrange
+    constexpr Vector<3> a = {1, 2, 3};
+    constexpr float expected = 3.74166f;
+    // act
+    const float norm = a.euclidianNorm();
+    // assert
+    ASSERT_TRUE(compare(norm, expected, 0.001f));
+}
+
+TEST(VectorGeneral, euclidian_norm_complex) {
+    // arrange
+    constexpr Vector<3, std::complex<float>> a = {{1, 2}, {3, 4}, {5, 6}};
+    constexpr float expected = 9.53939f;
+    // act
+    const float norm = a.euclidianNorm();
+    // assert
+    ASSERT_TRUE(compare(norm, expected, 0.001f));
+}
+
+TEST(VectorGeneral, euclidian_norm_squared_real) {
+    // arrange
+    constexpr Vector<3> a = {1, 2, 3};
+    constexpr float expected = 14;
+    // act
+    const float norm = a.euclidianNormSquared();
+    // assert
+    ASSERT_TRUE(compare(norm, expected, 0.001f));
+}
+
+TEST(VectorGeneral, euclidian_norm_squared_complex) {
+    // arrange
+    constexpr Vector<3, std::complex<float>> a = {{1, 2}, {3, 4}, {5, 6}};
+    constexpr float expected = 91;
+    // act
+    const float norm = a.euclidianNormSquared();
+    // assert
+    ASSERT_TRUE(compare(norm, expected, 0.001f));
+}
+
+TEST(VectorGeneral, max_norm_real) {
+    // arrange
+    constexpr Vector<3> a = {1, -3, 2};
+    constexpr float expected = 3;
+    // act
+    const float norm = a.maxNorm();
+    // assert
+    ASSERT_TRUE(compare(norm, expected, 0.001f));
+}
+
+TEST(VectorGeneral, max_norm_complex) {
+    // arrange
+    constexpr Vector<3, std::complex<float>> a = {{1, 2}, {-5, -6}, {3, 4}};
+    constexpr float expected = 7.81025f;
+    // act
+    const float norm = a.maxNorm();
+    // assert
+    ASSERT_TRUE(compare(norm, expected, 0.001f));
+}
