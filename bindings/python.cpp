5f225713793a2254fc7bb38367602ec117090b3a
Ricardo Baucells <ricardo@baucells.net>
2026-01-23 08:53:47 -0500

    Pyhton bindings with constructor and adding

--- bindings/python.cpp
+++ bindings/python.cpp
@@ -1,5 +1,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/native_enum.h>
+#include <pybind11/stl.h>
 #include "math++/math.h"
 
 namespace py = pybind11;
@@ -19,4 +20,39 @@ PYBIND11_MODULE(mathpy, m) {
     m.def("convert", [](const RotationType from, const RotationType to, const double rotation) -> double {
         return convert<double>(from, to, rotation);
     }, py::arg("from"), py::arg("to"), py::arg("rotation"));
+
+    py::class_<Matrix<3, 3, float>>(m, "Matrix3f")
+        .def(py::init([](const std::vector<std::vector<float>>& list) {
+            auto* matrix = new Matrix<3, 3, float>();
+
+            // number of rows in initializer list
+            if (list.size() != 3)
+                throw InvalidDimensionException("Incorrect number of rows in initializer list");
+            // row-major order in initializer list
+            int r = 0;
+            // loop over each row
+            for (const auto& row : list) {
+                // check correct number of elements in row
+                if (row.size() != 3)
+                    throw InvalidDimensionException("Incorrect number of columns in initializer list");
+                // column index
+                int c = 0;
+                // loop over each element in the row
+                for (const auto element : row) {
+                    // store in column-major order
+                    matrix->data[c][r] = element;
+                    // advance column
+                    c++;
+                }
+                // advance row
+                r++;
+            }
+
+            return matrix;
+        }))
+        .def("transpose", &Matrix<3, 3, float>::transpose)
+        .def("toString", &Matrix<3, 3, float>::toString)
+        .def("addEquals", [](Matrix<3, 3, float>& self, const Matrix<3, 3, float>& other) {
+            self.addEquals(other);
+        }, py::arg("other"));
 }
