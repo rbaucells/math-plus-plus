3584dd8b124c67ab96c991f959681d98de56b194
Ricardo Baucells <ricardo@baucells.net>
2026-03-14 01:24:01 -0400

    TEsts for SparseMatrix equality

---
+++ tests/matrix/sparse/operators/compare.tests.cpp
@@ -0,0 +1,135 @@
+#include "gtest/gtest.h"
+
+#include "mathpp/math.h"
+
+TEST(equality_operator, given_f_sparse_matrices_should_return_true) {
+    // arrange
+    const SparseMatrix<float> a(3, 3, {{1, 0, 0}, {6, 2, 1}, {9, 2, 2}});
+    const SparseMatrix<float> b(3, 3, {{1, 0, 0}, {6, 2, 1}, {9, 2, 2}});
+    const SparseMatrix<float> c(3, 3, {{1, 0, 0}, {6, 2, 1}, {9, 2, 2}});
+    // act
+    const bool result = a == b == c;
+    // assert
+    ASSERT_TRUE(result);
+}
+
+TEST(equality_operator, given_f_sparse_matrices_should_return_false) {
+    // arrange
+    const SparseMatrix<float> a(3, 3, {{1, 0, 0}, {3, 2, 1}, {9, 2, 2}});
+    const SparseMatrix<float> b(3, 3, {{1, 0, 0}, {6, 2, 1}, {9, 2, 2}});
+    const SparseMatrix<float> c(3, 3, {{1, 0, 0}, {6, 2, 1}, {9, 2, 2}});
+    // act
+    const bool result = a == b == c;
+    // assert
+    ASSERT_FALSE(result);
+}
+
+TEST(equality_operator, given_cf_sparse_matrices_should_return_true) {
+    // arrange
+    const SparseMatrix<std::complex<float>> a(3, 3, {{{1, 2}, 0, 0}, {{7, 8}, 2, 1}, {{13, 14}, 2, 2}});
+    const SparseMatrix<std::complex<float>> b(3, 3, {{{1, 2}, 0, 0}, {{7, 8}, 2, 1}, {{13, 14}, 2, 2}});
+    const SparseMatrix<std::complex<float>> c(3, 3, {{{1, 2}, 0, 0}, {{7, 8}, 2, 1}, {{13, 14}, 2, 2}});
+    // act
+    const bool result = a == b == c;
+    // assert
+    ASSERT_TRUE(result);
+}
+
+TEST(equality_operator, given_cf_sparse_matrices_should_return_false) {
+    // arrange
+    const SparseMatrix<std::complex<float>> a(3, 3, {{{1, 2}, 0, 0}, {{7, 8}, 2, 1}, {{13, 14}, 2, 2}});
+    const SparseMatrix<std::complex<float>> b(3, 3, {{{1, 2}, 0, 0}, {{7, 8}, 2, 1}, {{13, 14}, 2, 2}});
+    const SparseMatrix<std::complex<float>> c(3, 3, {{{1, 2}, 0, 0}, {{7, 8}, 2, 1}, {{13, 15}, 2, 2}});
+    // act
+    const bool result = a == b == c;
+    // assert
+    ASSERT_FALSE(result);
+}
+
+TEST(equality_operator, given_f_and_cf_sparse_matrices_should_return_true) {
+    // arrange
+    const SparseMatrix<float> a(3, 3, {{1, 0, 0}, {6, 2, 1}, {9, 2, 2}});
+    const SparseMatrix<std::complex<float>> b(3, 3, {{{1, 0}, 0, 0}, {{6, 0}, 2, 1}, {{9, 0}, 2, 2}});
+    const SparseMatrix<float> c(3, 3, {{1, 0, 0}, {6, 2, 1}, {9, 2, 2}});
+    // act
+    const bool result = a == b == c;
+    // assert
+    ASSERT_TRUE(result);
+}
+
+TEST(equality_operator, given_f_and_cf_sparse_matrices_should_return_false) {
+    // arrange
+    const SparseMatrix<float> a(3, 3, {{1, 0, 0}, {6, 2, 1}, {9, 2, 2}});
+    const SparseMatrix<std::complex<float>> b(3, 3, {{{1, 0}, 0, 0}, {{4, 0}, 2, 1}, {{9, 0}, 2, 2}});
+    const SparseMatrix<float> c(3, 3, {{1, 0, 0}, {6, 2, 1}, {9, 2, 2}});
+    // act
+    const bool result = a == b == c;
+    // assert
+    ASSERT_FALSE(result);
+}
+
+TEST(equality_operator, given_f_sparse_matrices_and_precision_should_return_true) {
+    // arrange
+    const SparseMatrix<float> a(3, 3, {{1, 0, 0}, {6, 2, 1}, {9, 2, 2}});
+    const SparseMatrix<float> b(3, 3, {{1, 0, 0}, {6.1f, 2, 1}, {9, 2, 2}});
+    const SparseMatrix<float> c(3, 3, {{1, 0, 0}, {6, 2, 1}, {9, 2, 2}});
+    // act
+    const bool result = (a == b == c) + Precision(0.1f);
+    // assert
+    ASSERT_TRUE(result);
+}
+
+TEST(equality_operator, given_f_sparse_matrices_and_precision_should_return_false) {
+    // arrange
+    const SparseMatrix<float> a(3, 3, {{1, 0, 0}, {6, 2, 1}, {9, 2, 2}});
+    const SparseMatrix<float> b(3, 3, {{1, 0, 0}, {6.1f, 2, 1}, {9, 2, 2}});
+    const SparseMatrix<float> c(3, 3, {{1, 0, 0}, {5.9f, 2, 1}, {9, 2, 2}});
+    // act
+    const bool result = (a == b == c) + Precision(0.1f);
+    // assert
+    ASSERT_FALSE(result);
+}
+
+TEST(equality_operator, given_cf_sparse_matrices_and_precision_should_return_true) {
+    // arrange
+    const SparseMatrix<std::complex<float>> a(3, 3, {{{1, 2}, 0, 0}, {{7, 8}, 2, 1}, {{13, 14}, 2, 2}});
+    const SparseMatrix<std::complex<float>> b(3, 3, {{{1, 2}, 0, 0}, {{7, 8.1f}, 2, 1}, {{13, 14}, 2, 2}});
+    const SparseMatrix<std::complex<float>> c(3, 3, {{{1, 2}, 0, 0}, {{7, 8}, 2, 1}, {{13, 14}, 2, 2}});
+    // act
+    const bool result = (a == b == c) + Precision(0.11f);
+    // assert
+    ASSERT_TRUE(result);
+}
+
+TEST(equality_operator, given_cf_sparse_matrices_and_precision_should_return_false) {
+    // arrange
+    const SparseMatrix<std::complex<float>> a(3, 3, {{{1, 2}, 0, 0}, {{7.1f, 8}, 2, 1}, {{13, 14}, 2, 2}});
+    const SparseMatrix<std::complex<float>> b(3, 3, {{{1, 2}, 0, 0}, {{7, 8}, 2, 1}, {{13, 14}, 2, 2}});
+    const SparseMatrix<std::complex<float>> c(3, 3, {{{0.9f, 2}, 0, 0}, {{7, 8}, 2, 1}, {{13, 14}, 2, 2}});
+    // act
+    const bool result = (a == b == c) + Precision(0.1f);
+    // assert
+    ASSERT_FALSE(result);
+}
+
+TEST(equality_operator, given_f_and_cf_sparse_matrices_and_precision_should_return_true) {
+    // arrange
+    const SparseMatrix<float> a(3, 3, {{1, 0, 0}, {6, 2, 1}, {9.1f, 2, 2}});
+    const SparseMatrix<std::complex<float>> b(3, 3, {{{1, 0}, 0, 0}, {{6, 0.1f}, 2, 1}, {{9, 0}, 2, 2}});
+    const SparseMatrix<float> c(3, 3, {{1, 0, 0}, {6, 2, 1}, {9, 2, 2}});
+    // act
+    const bool result = (a == b == c) + Precision(0.11f);
+    // assert
+    ASSERT_TRUE(result);
+}
+
+TEST(equality_operator, given_f_and_cf_sparse_matrices_and_precision_should_return_false) {
+    // arrange
+    const SparseMatrix<float> a(3, 3, {{1, 0, 0}, {6, 2, 1}, {9.1f, 2, 2}});
+    const SparseMatrix<std::complex<float>> b(3, 3, {{{1, 0}, 0, 0}, {{4, 0.1f}, 2, 1}, {{9, 0}, 2, 2}});
+    const SparseMatrix<float> c(3, 3, {{1, 0, 0}, {6, 2, 1}, {8.9f, 2, 2}});
+    // act
+    const bool result = (a == b == c) + Precision(0.1f);
+    // assert
+    ASSERT_FALSE(result);
+}
