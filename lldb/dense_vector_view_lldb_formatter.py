8753a1f136e43d5e3645a3f285d63f031a332f34
Ricardo Baucells <ricardo@baucells.net>
2026-03-16 16:47:19 -0400

    Simplified dense_vector_view_lldb_formatter.py.py

--- lldb/dense_vector_view_lldb_formatter.py
+++ lldb/dense_vector_view_lldb_formatter.py
@@ -4,66 +4,36 @@ import lldb
 def dense_vector_view_summary(valobj: lldb.SBValue, internal_dict):
     valobj = valobj.GetNonSyntheticValue()
 
-    dense_vector_view_type: lldb.SBType = valobj.GetType()
-
-    if not dense_vector_view_type.IsValid():
-        raise RuntimeError("dense_vector_view_type is invalid")
-
-    dense_vector_view_type = get_real_type(dense_vector_view_type)
-
-    t_type = dense_vector_view_type.GetTemplateArgumentType(0)
-
-    if not t_type.IsValid():
-        raise RuntimeError("t_type is invalid")
-
-    scalar_type = scalar_type_from_type(t_type)
-
+    dense_vector_view_type: lldb.SBType = get_real_type(valobj.GetType())
+    scalar_type: ScalarType = scalar_type_from_type(dense_vector_view_type.GetTemplateArgumentType(0))
     n: lldb.SBValue = valobj.GetChildMemberWithName("n")
-
-    if not n.IsValid():
-        raise RuntimeError("n is invalid")
-
-    n_int = n.GetValueAsUnsigned()
-
-    print(f"n_int = {n_int}")
+    n_int: int = n.GetValueAsUnsigned()
 
     if n_int == 0:
-        raise RuntimeError("empty dense vector view")
+        return "Empty Vector (n = 0)"
 
     if n_int > 7:
-        return ""
+        return f"Vector too big for summary (n = {n}"
 
     offset: lldb.SBValue = valobj.GetChildMemberWithName("offset_")
-
-    if not offset.IsValid():
-        raise RuntimeError("offset is invalid")
-
     offset_int = offset.GetValueAsUnsigned()
 
-    print(f"offset_int = {offset_int}")
-
     owner: lldb.SBValue = valobj.GetChildMemberWithName("owner_")
-
-    if not owner.IsValid():
-        raise RuntimeError("owner is invalid")
-
     data: lldb.SBValue = owner.GetChildMemberWithName("data_")
 
-    if not data.IsValid():
-        raise RuntimeError("owner's data member is not valid")
-
     if data.GetValueAsSigned() == 0:
-        raise RuntimeError("owner's data member is nullptr")
+        return "View on null Vector (owner.data_ = nullptr)"
 
     summary: str = "{"
 
     for i in range(0, n_int):
         cur_element_data: lldb.SBValue = iterate_data_array(data, i + offset_int)
 
-        if not cur_element_data.IsValid():
-            raise RuntimeError(f"cur_element at index = {i}) is not valid")
+        if cur_element_data.IsValid():
+            cur_element = get_str_from_value(cur_element_data, scalar_type)
+        else:
+            cur_element = "N/A"
 
-        cur_element = get_str_from_value(cur_element_data, scalar_type)
 
         if i != n_int - 1:
             summary += f"{cur_element}, "
@@ -76,38 +46,17 @@ def dense_vector_view_summary(valobj: lldb.SBValue, internal_dict):
 
 class DenseVectorViewSyntheticChildrenProvider:
     def __init__(self, valobj: lldb.SBValue, internal_dict):
+        self.valobj = valobj
+
         self.n = valobj.GetChildMemberWithName("n")
-
-        if not self.n.IsValid():
-            raise RuntimeError("n is invalid")
-
         self.n_int = self.n.GetValueAsUnsigned()
 
         self.offset = valobj.GetChildMemberWithName("offset_")
-
-        if not self.offset.IsValid():
-            raise RuntimeError("offset_ is invalid")
-
         self.offset_int = self.offset.GetValueAsUnsigned()
 
-        self.valobj = valobj
+        self.owner = valobj.GetChildMemberWithName("owner_")
 
-        owner: lldb.SBValue = valobj.GetChildMemberWithName("owner_")
-
-        if not owner.IsValid():
-            raise RuntimeError("owner is invalid")
-
-        self.owner = owner
-
-        data: lldb.SBValue = owner.GetChildMemberWithName("data_")
-
-        if not data.IsValid():
-            raise RuntimeError("owner's data member is not valid")
-
-        if data.GetValueAsSigned() == 0:
-            raise RuntimeError("owner's data member is nullptr")
-
-        self.data = data
+        self.data = self.owner.GetChildMemberWithName("data_")
 
         self.element_type = self.data.GetType().GetPointeeType()
         self.array_type = self.element_type.GetArrayType(self.n_int)
@@ -140,7 +89,7 @@ class DenseVectorViewSyntheticChildrenProvider:
         if index == 2:
             return self.data.CreateValueFromAddress(
                 "view",
-                self.data.GetValueAsUnsigned(),
+                self.data.GetValueAsUnsigned() + (self.offset_int * self.element_type.GetByteSize()),
                 self.array_type
             )
 
@@ -150,32 +99,13 @@ class DenseVectorViewSyntheticChildrenProvider:
         return None
 
 def to_string_dvv(debugger: lldb.SBDebugger, command: str, result: lldb.SBCommandReturnObject, internal_dict):
-    target: lldb.SBTarget = debugger.GetSelectedTarget()
-
-    if not target.IsValid():
-        result.PutError("target was invalid")
-        return
-
-    frame: lldb.SBFrame = target.GetProcess().GetSelectedThread().GetSelectedFrame()
-
-    if not frame.IsValid():
-        result.PutError("frame was invalid")
-        return
-
-    valobj: lldb.SBValue = frame.FindVariable(command)
-
-    if not valobj.IsValid():
-        result.PutError("valobj was invalid")
-        return
-
-    valobj = valobj.GetNonSyntheticValue()
+    frame: lldb.SBFrame = debugger.GetSelectedTarget().GetProcess().GetSelectedThread().GetSelectedFrame()
+    valobj: lldb.SBValue = frame.FindVariable(command).GetNonSyntheticValue()
 
     summary = dense_vector_view_summary(valobj, internal_dict)
-
     if vector_to_string_orientation == "vertical":
         summary = summary.replace("{", "{\n ")
         summary = summary.replace("}", "\n}")
         summary = summary.replace(",", "\n")
 
     result.PutCString(summary)
-    return None
