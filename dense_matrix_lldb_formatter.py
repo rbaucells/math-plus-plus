dabefcbabba3785b54d966c3a06b320637914d8c
Ricardo Baucells <ricardo@baucells.net>
2026-03-16 10:11:38 -0400

    Synthetic children for matrix

--- dense_matrix_lldb_formatter.py
+++ dense_matrix_lldb_formatter.py
@@ -5,8 +5,8 @@ precision: int = 2
 imag_char: str = 'i'
 
 def __lldb_init_module(debugger: lldb.SBDebugger, dict):
-    debugger.HandleCommand(
-        f'type summary add -x "^DenseMatrix<.*>$" -F dense_matrix_lldb_formatter.dense_matrix_summary')
+    debugger.HandleCommand(f'type summary add -x "^DenseMatrix<.*>$" -F dense_matrix_lldb_formatter.dense_matrix_summary')
+    debugger.HandleCommand(f'type synthetic add -x "^DenseMatrix<.*>$" --python-class dense_matrix_lldb_formatter.DenseMatrixSyntheticChildrenProvider')
     print("Imported dense_matrix_lldb_formatter.py")
 
 
@@ -110,6 +110,8 @@ def iterate_data_array(data_ptr: lldb.SBValue, index: int) -> lldb.SBValue:
 
 
 def dense_matrix_summary(valobj: lldb.SBValue, internal_dict):
+    valobj = valobj.GetNonSyntheticValue()
+
     dense_matrix_type: lldb.SBType = valobj.GetType()
 
     if not dense_matrix_type.IsValid():
@@ -177,3 +179,64 @@ def dense_matrix_summary(valobj: lldb.SBValue, internal_dict):
     summary += "}"
 
     return summary
+
+class DenseMatrixSyntheticChildrenProvider:
+    def __init__(self, valobj: lldb.SBValue, internal_dict):
+        columns: lldb.SBValue = valobj.GetChildMemberWithName("columns")
+
+        if not columns.IsValid():
+            raise RuntimeError("columns member is not valid")
+
+        rows: lldb.SBValue = valobj.GetChildMemberWithName("rows")
+
+        if not rows.IsValid():
+            raise RuntimeError("rows member is not valid")
+
+        self.columns = columns
+        self.rows = rows
+
+        self.columns_int = columns.GetValueAsSigned()
+        self.rows_int = rows.GetValueAsSigned()
+
+        self.valobj = valobj
+
+        data: lldb.SBValue = valobj.GetChildMemberWithName("data_")
+
+        if not data.IsValid():
+            raise RuntimeError("data member is not valid")
+
+        self.data = data
+        self.element_type = self.data.GetType().GetPointeeType()
+
+        self.total_elements = self.rows_int * self.columns_int
+        self.array_type = self.element_type.GetArrayType(self.total_elements)
+
+    def num_children(self, max_children: int) -> int:
+        return 3
+
+    def get_child_index(self, name: str) -> int:
+        if name == "columns":
+            return 0
+
+        if name == "rows":
+            return 1
+
+        if name == "data":
+            return 2
+
+        return -1
+
+    def get_child_at_index(self, index: int):
+        if index == 0:
+            return self.columns
+
+        if index == 1:
+            return self.rows
+
+        if index == 2:
+            return self.data.CreateValueFromAddress(
+                "data",
+                self.data.GetValueAsUnsigned(),
+                self.array_type
+            )
+        return None
