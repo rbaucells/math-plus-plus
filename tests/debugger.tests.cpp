c1f69eccd099f42a095e96967b073941c2742c8f
Ricardo Baucells <ricardo@baucells.net>
2026-03-31 07:48:20 -0400

    Update debugger tests to use std::size_t

--- tests/debugger.tests.cpp
+++ tests/debugger.tests.cpp
@@ -46,13 +46,13 @@ TEST(debugger_tests, sparse_matrice_views) {
 }
 
 TEST(debugger_tests, custom_sparse_matrices) {
-    int* colOffsets = new int[4];
+    std::size_t* colOffsets = new std::size_t[4];
     colOffsets[0] = 0;
     colOffsets[1] = 2;
     colOffsets[2] = 3;
     colOffsets[3] = 4;
 
-    int* rowIndices = new int[4];
+    std::size_t* rowIndices = new std::size_t[4];
     rowIndices[0] = 0;
     rowIndices[1] = 2;
     rowIndices[2] = 1;
@@ -64,15 +64,15 @@ TEST(debugger_tests, custom_sparse_matrices) {
     values[2] = 8;
     values[3] = 6;
 
-    int nnz = 4;
+    std::size_t nnz = 4;
     CustomSparseMatrix<float> a(3, 3, colOffsets, rowIndices, values, nnz);
 
-    int* colOffsets2 = new int[3];
+    std::size_t* colOffsets2 = new std::size_t[3];
     colOffsets2[0] = 0;
     colOffsets2[1] = 1;
     colOffsets2[2] = 3;
 
-    int* rowIndices2 = new int[3];
+    std::size_t* rowIndices2 = new std::size_t[3];
     rowIndices2[0] = 1;
     rowIndices2[1] = 0;
     rowIndices2[2] = 2;
@@ -82,16 +82,16 @@ TEST(debugger_tests, custom_sparse_matrices) {
     values2[1] = 7;
     values2[2] = 9;
 
-    int nnz2 = 3;
+    std::size_t nnz2 = 3;
     CustomSparseMatrix<float> b(3, 3, colOffsets2, rowIndices2, values2, nnz2);
 
-    int* colOffsets3 = new int[4];
+    std::size_t* colOffsets3 = new std::size_t[4];
     colOffsets3[0] = 0;
     colOffsets3[1] = 1;
     colOffsets3[2] = 2;
     colOffsets3[3] = 4;
 
-    int* rowIndices3 = new int[4];
+    std::size_t* rowIndices3 = new std::size_t[4];
     rowIndices3[0] = 0;
     rowIndices3[1] = 1;
     rowIndices3[2] = 0;
@@ -103,7 +103,7 @@ TEST(debugger_tests, custom_sparse_matrices) {
     values3[2] = 5;
     values3[3] = 3;
 
-    int nnz3 = 4;
+    std::size_t nnz3 = 4;
     CustomSparseMatrix<float> c(3, 3, colOffsets3, rowIndices3, values3, nnz3);
 }
 
@@ -138,11 +138,11 @@ TEST(debugger_tests, custom_sparse_vectors) {
     values[1] = 3;
     values[2] = 5;
 
-    int* indices = new int[3];
+    std::size_t* indices = new std::size_t[3];
     indices[0] = 0;
     indices[1] = 2;
     indices[2] = 4;
 
-    int nnz = 3;
+    std::size_t nnz = 3;
     CustomSparseVector<float> a(5, values, indices, nnz);
 }
