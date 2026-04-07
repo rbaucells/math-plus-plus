04df9fd261b8108c01e62b4a94b15cd9425acdaa
Ricardo Baucells <ricardo@baucells.net>
2026-04-07 13:12:08 -0400

    Proper function comments for scalar compare funcitons

--- src/helper.h
+++ src/helper.h
@@ -146,28 +146,28 @@ struct Precision {
 
 
 /**
- * @brief Checks if 'a, b, and args' are all equal within 'precision'.
- * @return Whether 'a, b, and args' are all equal within 'precision'.
- * @param precision Precision type on real scalar of precision to compare numbers within.
- * @param a First argument to compare.
- * @param b Second argument to compare.
- * @param args Optional rest of arguments to compare.
- * @tparam T Scalar types of 'a'.
- * @tparam U Scalar types of 'b'.
+ * @brief Compares arguments up to the specified precision.
+ *
+ * @tparam T Scalar type of 'a'.
+ * @tparam U Scalar type of 'b'.
  * @tparam ARGS Scalar types of 'args'.
- * @note The underlying types of 'T, U, and ARGS' must have a common type.
- * @note compare(Precision(0.1f), 1.0f, 1.1f) will return true. (inclusive).
+ * @param precision How precise the comparison should be. (inclusive).
+ * @param a First scalar argument.
+ * @param b Second scalar argument.
+ * @param args Remaining scalar arguments.
+ *
+ * @return Whether all arguments are equal up to the specified precision.
+ * @note Inclusive comparison, meaning 1, 0.9, and 1.1 are all equal up to a precision of 0.1
  */
 template<scalar T, scalar U, scalar... ARGS> requires has_common_type<underlying_type_t<T>, underlying_type_t<U>, underlying_type_t<ARGS>...>
 [[nodiscard]] bool compare(const Precision<std::common_type_t<underlying_type_t<T>, underlying_type_t<U>, underlying_type_t<ARGS>...>> precision, const T a, const U b, const ARGS... args) {
-    // they are all real
-    if constexpr (real<T> && real<U> && (real<ARGS> && ...)) {
+    if constexpr (real<T> && real<U> && (real<ARGS> && ...)) { // get min and max, then compare withing precision
         const std::common_type_t<T, U, ARGS...> min = std::min({static_cast<std::common_type_t<T, U, ARGS...>>(a), static_cast<std::common_type_t<T, U, ARGS...>>(b), (static_cast<std::common_type_t<T, U, ARGS...>>(args))...});
         const std::common_type_t<T, U, ARGS...> max = std::max({static_cast<std::common_type_t<T, U, ARGS...>>(a), static_cast<std::common_type_t<T, U, ARGS...>>(b), (static_cast<std::common_type_t<T, U, ARGS...>>(args))...});
 
         return max - min <= precision.value;
     }
-    else {
+    else { // get min and max of real and imag parts separately, then compare withing precision
         const std::common_type_t<underlying_type_t<T>, underlying_type_t<U>, underlying_type_t<ARGS>...> realMin = std::min({static_cast<std::common_type_t<underlying_type_t<T>, underlying_type_t<U>, underlying_type_t<ARGS>...>>(std::real(a)), static_cast<std::common_type_t<underlying_type_t<T>, underlying_type_t<U>, underlying_type_t<ARGS>...>>(std::real(b)), (static_cast<std::common_type_t<underlying_type_t<T>, underlying_type_t<U>, underlying_type_t<ARGS>...>>(std::real(args)))...});
         const std::common_type_t<underlying_type_t<T>, underlying_type_t<U>, underlying_type_t<ARGS>...> realMax = std::max({static_cast<std::common_type_t<underlying_type_t<T>, underlying_type_t<U>, underlying_type_t<ARGS>...>>(std::real(a)), static_cast<std::common_type_t<underlying_type_t<T>, underlying_type_t<U>, underlying_type_t<ARGS>...>>(std::real(b)), (static_cast<std::common_type_t<underlying_type_t<T>, underlying_type_t<U>, underlying_type_t<ARGS>...>>(std::real(args)))...});
 
@@ -187,15 +187,18 @@ template<scalar T, scalar U, scalar... ARGS> requires has_common_type<underlying
 }
 
 /**
- * @brief Checks if 'a, b, and args' are all equal within machine precision.
- * @return Whether 'a, b, and args' are all equal within machine precision.
- * @param a First argument to compare.
- * @param b Second argument to compare.
- * @param args Optional rest of arguments to compare.
+ * @brief Compares arguments up to machine epsilon precision.
+ * @details Calls compare(precision, a, b, args) with machine epsilon precision.
+ *
  * @tparam T Scalar type of 'a'.
  * @tparam U Scalar type of 'b'.
  * @tparam ARGS Scalar types of 'args'.
- * @note The underlying types of 'T, U, and ARGS' must have a common type.
+ * @param a First scalar argument.
+ * @param b Second scalar argument.
+ * @param args Remaining scalar arguments.
+ *
+ * @return Whether all arguments are equal up to machine epsilon precision.
+ * @see compare(const Precision<std::common_type_t<underlying_type_t<T>, underlying_type_t<U>, underlying_type_t<ARGS>...>> precision, const T a, const U b, const ARGS... args)
  */
 template<scalar T, scalar U, scalar... ARGS> requires has_common_type<underlying_type_t<T>, underlying_type_t<U>, underlying_type_t<ARGS>...>
 [[nodiscard]] bool compare(const T a, const U b, const ARGS... args) {
