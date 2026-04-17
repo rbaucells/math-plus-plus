90f055e148e9765b34f89154d25e1659f4252b98
Ricardo Baucells <ricardo@baucells.net>
2026-04-17 07:29:58 -0400

    Refactor subtract function to return expression

--- src/matrix/dense-refactor/operators/subtract.h
+++ src/matrix/dense-refactor/operators/subtract.h
@@ -2,24 +2,6 @@
 #include "../matrix.h"
 #include "../../../helper.h"
 
-template<dense_matrix_like T, dense_matrix_like U, dense_matrix_like... ARGS> requires has_common_type<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>
-DenseMatrix<std::common_type_t<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>> subtract(const T& a, const U& b, const ARGS&... args) {
-    assert_same_dimensions(a, b, args...);
-
-    const int columns = a.columns();
-    const int rows = a.rows();
-
-    DenseMatrix<std::common_type_t<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>> result(rows, columns, false);
-
-    for (int c = 0; c < columns; c++) {
-        for (int r = 0; r < rows; r++) {
-            result[c, r] = ((a[c, r] - b[c, r]) - ... - args[c, r]);
-        }
-    }
-
-    return result;
-}
-
 template<dense_matrix_like... ARGS> requires has_common_type<typename ARGS::ValueType...>
 struct DenseMatrixSubtractExpr {
     using ValueType = std::common_type_t<typename ARGS::ValueType...>;
@@ -67,7 +49,14 @@ struct DenseMatrixSubtractExpr {
     }
 };
 
+template<dense_matrix_like T, dense_matrix_like U, dense_matrix_like... ARGS> requires has_common_type<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>
+DenseMatrixSubtractExpr<T, U, ARGS...> subtract(const T& a, const U& b, const ARGS&... args) {
+    assert_same_dimensions(a, b, args...);
+    return DenseMatrixSubtractExpr<T, U, ARGS...>(a, b, args...);
+}
+
 template<dense_matrix_like T, dense_matrix_like U> requires has_common_type<typename T::ValueType, typename U::ValueType>
 DenseMatrixSubtractExpr<T, U> operator-(const T& a, const U& b) {
+    assert_same_dimensions(a, b);
     return DenseMatrixSubtractExpr<T, U>(a, b);
 }
