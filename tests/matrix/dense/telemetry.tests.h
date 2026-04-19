729f052518a5e8b4dc3cba0b495cb1d2953aa65a
Ricardo Baucells <ricardo@baucells.net>
2026-04-19 19:30:21 -0400

    Improve syntax of run_with_budget

--- tests/matrix/dense/telemetry.tests.h
+++ tests/matrix/dense/telemetry.tests.h
@@ -75,10 +75,10 @@ protected:
     }
 };
 
-template<typename RESULT, typename ACTION>
-RESULT run_with_budget(const DenseMatrixStats& expected, ACTION&& action) {
+template<typename ACTION>
+auto run_with_budget(const DenseMatrixStats& expected, ACTION&& action) {
     const DenseMatrixTelemetryScope scope;
-    RESULT result(std::forward<ACTION>(action)());
+    auto result(std::forward<ACTION>(action)());
     scope.assert_stats_delta(expected);
     return result;
 }
