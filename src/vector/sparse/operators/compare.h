095c0f4b4b4723693aa820ef729c2d0282de0be9
Ricardo Baucells <ricardo@baucells.net>
2026-04-20 13:25:58 -0400

    refactor sparse vector compare

--- src/vector/sparse/operators/compare.h
+++ src/vector/sparse/operators/compare.h
@@ -1,24 +1,23 @@
 #pragma once
-#include "../vector.h"
 #include "../../../helper.h"
+#include "../vector.h"
 
-template<sparse_vector_base T, sparse_vector_base U, sparse_vector_base... ARGS> requires has_common_type<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>
+template<sparse_vector_like T, sparse_vector_like U, sparse_vector_like... ARGS> requires has_common_type<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>
 [[nodiscard]] bool compare(const T& a, const U& b, const ARGS&... args) {
     return compare(Precision(epsilon<underlying_type_t<std::common_type_t<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>>>()), a, b, args...);
 }
 
-template<sparse_vector_base T, sparse_vector_base U, sparse_vector_base... ARGS> requires has_common_type<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>
+template<sparse_vector_like T, sparse_vector_like U, sparse_vector_like... ARGS> requires has_common_type<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>
 [[nodiscard]] bool compare(const Precision<underlying_type_t<std::common_type_t<typename T::ValueType, typename ARGS::ValueType...>>> precision, const T& a, const U& b, const ARGS&... args) {
     assert_same_size(a, b, args...);
 
-    // if nnz are different, there is no way they are the same
     if (!compare(a.nnz(), b.nnz(), args.nnz()...)) {
         return false;
     }
 
-    const int n = a.n();
+    const std::size_t n = a.n();
 
-    for (int i = 0; i < n; i++) {
+    for (std::size_t i = 0; i < n; i++) {
         if (!compare(precision, a.get(i), b.get(i), args.get(i)...)) {
             return false;
         }
@@ -27,23 +26,29 @@ template<sparse_vector_base T, sparse_vector_base U, sparse_vector_base... ARGS>
     return true;
 }
 
-template<sparse_vector_base... ARGS> requires has_common_type<typename ARGS::ValueType...>
-struct SparseVectorCompareExpr : Expression<bool, ARGS...> {
+template<sparse_vector_like... ARGS> requires has_common_type<typename ARGS::ValueType...>
+struct SparseVectorCompareExpr {
+    std::tuple<const ARGS&...> args;
+
     Precision<underlying_type_t<std::common_type_t<typename ARGS::ValueType...>>> precision;
 
-    SparseVectorCompareExpr(const Precision<underlying_type_t<std::common_type_t<typename ARGS::ValueType...>>> precision, const ARGS&... args) : Expression<bool, ARGS...>(args...), precision(precision) {}
+    explicit SparseVectorCompareExpr(const Precision<underlying_type_t<std::common_type_t<typename ARGS::ValueType...>>> precision, const ARGS&... args) : args(args...), precision(precision) {
+    }
 
-
-    [[nodiscard]] bool evaluate() const override {
-        return std::apply([this](const auto&... args) {
-            return compare(precision, args...);
+    [[nodiscard]] bool evaluate() const {
+        return std::apply([this](const auto&... m) {
+            return compare(precision, m...);
         }, this->args);
     }
 
-    template<sparse_vector_base OTHER> requires has_common_type<typename ARGS::ValueType..., typename OTHER::ValueType>
+    operator bool() const {
+        return evaluate();
+    }
+
+    template<sparse_vector_like OTHER> requires has_common_type<typename ARGS::ValueType..., typename OTHER::ValueType>
     SparseVectorCompareExpr<ARGS..., OTHER> operator==(const OTHER& other) const {
-        return std::apply([&](const auto&... args) {
-            return SparseVectorCompareExpr<ARGS..., OTHER>(precision, args..., other);
+        return std::apply([&](const auto&... m) {
+            return SparseVectorCompareExpr<ARGS..., OTHER>(precision, m..., other);
         }, this->args);
     }
 
@@ -53,7 +58,7 @@ struct SparseVectorCompareExpr : Expression<bool, ARGS...> {
     }
 };
 
-template<sparse_vector_base T, sparse_vector_base U>
+template<sparse_vector_like T, sparse_vector_like U> requires has_common_type<typename T::ValueType, typename U::ValueType>
 SparseVectorCompareExpr<T, U> operator==(const T& a, const U& b) {
     return SparseVectorCompareExpr<T, U>(Precision(epsilon<underlying_type_t<std::common_type_t<typename T::ValueType, typename U::ValueType>>>()), a, b);
 }
