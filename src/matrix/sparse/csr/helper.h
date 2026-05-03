f8570854df74ea20362a7a8358ca033e7f4f164e
Ricardo Baucells <ricardo@baucells.net>
2026-05-03 14:35:33 -0400

    copy into csr

---
+++ src/matrix/sparse/csr/helper.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "../../../helper.h"
+#include <cstddef>
+#include <concepts>
+#include <type_traits>
+#include "../../../exceptions.h"
+
+template<scalar T>
+struct CSRSparseMatrix;
+
+template<scalar T>
+struct CSRSparseMatrixView;
+
+template<scalar T>
+struct CSRCustomSparseMatrix;
+
+// is_csr_sparse_matrix, is_csr_sparse_matrix_v, csr_sparse_matrix
+template<typename>
+struct is_csr_sparse_matrix : std::false_type {};
+
+template<typename U>
+struct is_csr_sparse_matrix<CSRSparseMatrix<U>> : std::true_type {};
+
+template<typename T>
+inline constexpr bool is_csr_sparse_matrix_v = is_csr_sparse_matrix<T>::value;
+
+template<typename T>
+concept csr_sparse_matrix = is_csr_sparse_matrix_v<T>;
+
+// is_csr_sparse_matrix_view, is_csr_sparse_matrix_view_v, csr_sparse_matrix_view
+template<typename>
+struct is_csr_sparse_matrix_view : std::false_type {};
+
+template<typename U>
+struct is_csr_sparse_matrix_view<CSRSparseMatrixView<U>> : std::true_type {};
+
+template<typename T>
+inline constexpr bool is_csr_sparse_matrix_view_v = is_csr_sparse_matrix_view<T>::value;
+
+template<typename T>
+concept csr_sparse_matrix_view = is_csr_sparse_matrix_view_v<T>;
+
+// is_csr_custom_sparse_matrix, is_csr_custom_sparse_matrix_v, csr_custom_sparse_matrix
+template<typename>
+struct is_csr_custom_sparse_matrix : std::false_type {};
+
+template<typename U>
+struct is_csr_custom_sparse_matrix<CSRCustomSparseMatrix<U>> : std::true_type {};
+
+template<typename T>
+inline constexpr bool is_csr_custom_sparse_matrix_v = is_csr_custom_sparse_matrix<T>::value;
+
+template<typename T>
+concept custom_sparse_csr_matrix = is_csr_custom_sparse_matrix_v<T>;
