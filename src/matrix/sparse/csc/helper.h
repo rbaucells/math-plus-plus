1c4026453ebf934746512f2ea6af80bd7957e2bd
Ricardo Baucells <ricardo@baucells.net>
2026-05-03 00:09:08 -0400

    rename concepts

--- src/matrix/sparse/csc/helper.h
+++ src/matrix/sparse/csc/helper.h
@@ -14,41 +14,41 @@ struct CSCSparseMatrixView;
 template<scalar T>
 struct CSCCustomSparseMatrix;
 
-// is_csc_matrix, is_csc_matrix_v, csc_matrix
+// is_csc_sparse_matrix, is_csc_sparse_matrix_v, csc_sparse_matrix
 template<typename>
-struct is_csc_matrix : std::false_type {};
+struct is_csc_sparse_matrix : std::false_type {};
 
 template<typename U>
-struct is_csc_matrix<CSCSparseMatrix<U>> : std::true_type {};
+struct is_csc_sparse_matrix<CSCSparseMatrix<U>> : std::true_type {};
 
 template<typename T>
-inline constexpr bool is_csc_matrix_v = is_csc_matrix<T>::value;
+inline constexpr bool is_csc_sparse_matrix_v = is_csc_sparse_matrix<T>::value;
 
 template<typename T>
-concept csc_matrix = is_csc_matrix_v<T>;
+concept csc_sparse_matrix = is_csc_sparse_matrix_v<T>;
 
-// is_csc_matrix_view, is_csc_matrix_view_v, csc_matrix_view
+// is_csc_sparse_matrix_view, is_csc_sparse_matrix_view_v, csc_sparse_matrix_view
 template<typename>
-struct is_csc_matrix_view : std::false_type {};
+struct is_csc_sparse_matrix_view : std::false_type {};
 
 template<typename U>
-struct is_csc_matrix_view<CSCSparseMatrixView<U>> : std::true_type {};
+struct is_csc_sparse_matrix_view<CSCSparseMatrixView<U>> : std::true_type {};
 
 template<typename T>
-inline constexpr bool is_csc_matrix_view_v = is_csc_matrix_view<T>::value;
+inline constexpr bool is_csc_sparse_matrix_view_v = is_csc_sparse_matrix_view<T>::value;
 
 template<typename T>
-concept csc_matrix_view = is_csc_matrix_view_v<T>;
+concept csc_sparse_matrix_view = is_csc_sparse_matrix_view_v<T>;
 
-// is_csc_custom_matrix, is_csc_custom_matrix_v, csc_custom_matrix
+// is_csc_custom_sparse_matrix, is_csc_custom_sparse_matrix_v, csc_custom_sparse_matrix
 template<typename>
-struct is_csc_custom_matrix : std::false_type {};
+struct is_csc_custom_sparse_matrix : std::false_type {};
 
 template<typename U>
-struct is_csc_custom_matrix<CSCCustomSparseMatrix<U>> : std::true_type {};
+struct is_csc_custom_sparse_matrix<CSCCustomSparseMatrix<U>> : std::true_type {};
 
 template<typename T>
-inline constexpr bool is_csc_custom_matrix_v = is_csc_custom_matrix<T>::value;
+inline constexpr bool is_csc_custom_sparse_matrix_v = is_csc_custom_sparse_matrix<T>::value;
 
 template<typename T>
-concept custom_csc_matrix = is_csc_custom_matrix_v<T>;
+concept custom_sparse_csc_matrix = is_csc_custom_sparse_matrix_v<T>;
