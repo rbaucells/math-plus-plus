2ea4dba5c0fa601c5f2cd221f314b9fdfb2624e3
Ricardo Baucells <ricardo@baucells.net>
2026-05-02 21:14:55 -0400

    refactor sparsr matrix compare

--- src/matrix/sparse/operators/compare.h
+++ src/matrix/sparse/operators/compare.h
@@ -3,28 +3,27 @@
 #include <type_traits>
 #include "../../../helper.h"
 #include <tuple>
-#include "../../../expression.h"
+#include <cstddef>
 
-template<sparse_matrix_base T, sparse_matrix_base U, sparse_matrix_base... ARGS> requires has_common_type<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>
+template<sparse_matrix_like T, sparse_matrix_like U, sparse_matrix_like... ARGS> requires has_common_type<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>
 [[nodiscard]] bool compare(const T& a, const U& b, const ARGS&... args) {
     return compare(Precision(epsilon<underlying_type_t<std::common_type_t<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>>>()), a, b, args...);
 }
 
-template<sparse_matrix_base T, sparse_matrix_base U, sparse_matrix_base... ARGS> requires has_common_type<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>
+template<sparse_matrix_like T, sparse_matrix_like U, sparse_matrix_like... ARGS> requires has_common_type<typename T::ValueType, typename U::ValueType, typename ARGS::ValueType...>
 [[nodiscard]] bool compare(const Precision<underlying_type_t<std::common_type_t<typename T::ValueType, typename ARGS::ValueType...>>> precision, const T& a, const U& b, const ARGS&... args) {
     assert_same_dimensions(a, b, args...);
 
-    // if nnz are different, there is no way they are the same
     if (!compare(a.nnz(), b.nnz(), args.nnz()...)) {
         return false;
     }
 
-    const int columns = a.columns();
-    const int rows = a.rows();
+    const std::size_t columns = a.columns();
+    const std::size_t rows = a.rows();
 
-    for (int c = 0; c < columns; c++) {
-        for (int r = 0; r < rows; r++) {
-            if (!compare(precision, a.get(c, r), b.get(c, r), args.get(c, r)...)) {
+    for (std::size_t c = 0; c < columns; c++) {
+        for (std::size_t r = 0; r < rows; r++) {
+            if (!compare(precision, a.get(r, c), b.get(r, c), args.get(r, c)...)) {
                 return false;
             }
         }
@@ -33,23 +32,29 @@ template<sparse_matrix_base T, sparse_matrix_base U, sparse_matrix_base... ARGS>
     return true;
 }
 
-template<sparse_matrix_base... ARGS> requires has_common_type<typename ARGS::ValueType...>
-struct SparseMatrixCompareExpr : Expression<bool, ARGS...> {
+template<sparse_matrix_like... ARGS> requires has_common_type<typename ARGS::ValueType...>
+struct SparseMatrixCompareExpr {
+    std::tuple<const ARGS&...> args;
+
     Precision<underlying_type_t<std::common_type_t<typename ARGS::ValueType...>>> precision;
 
-    SparseMatrixCompareExpr(const Precision<underlying_type_t<std::common_type_t<typename ARGS::ValueType...>>> precision, const ARGS&... args) : Expression<bool, ARGS...>(args...), precision(precision) {}
+    explicit SparseMatrixCompareExpr(const Precision<underlying_type_t<std::common_type_t<typename ARGS::ValueType...>>> precision, const ARGS&... args) : args(args...), precision(precision) {
+    }
 
-
-    [[nodiscard]] bool evaluate() const override {
-        return std::apply([this](const auto&... args) {
-            return compare(precision, args...);
+    [[nodiscard]] bool evaluate() const {
+        return std::apply([this](const auto&... m) {
+            return compare(precision, m...);
         }, this->args);
     }
 
-    template<sparse_matrix_base OTHER> requires has_common_type<typename ARGS::ValueType..., typename OTHER::ValueType>
+    operator bool() const {
+        return evaluate();
+    }
+
+    template<sparse_matrix_like OTHER> requires has_common_type<typename ARGS::ValueType..., typename OTHER::ValueType>
     SparseMatrixCompareExpr<ARGS..., OTHER> operator==(const OTHER& other) const {
-        return std::apply([&](const auto&... args) {
-            return SparseMatrixCompareExpr<ARGS..., OTHER>(precision, args..., other);
+        return std::apply([&](const auto&... m) {
+            return SparseMatrixCompareExpr<ARGS..., OTHER>(precision, m..., other);
         }, this->args);
     }
 
@@ -59,7 +64,7 @@ struct SparseMatrixCompareExpr : Expression<bool, ARGS...> {
     }
 };
 
-template<sparse_matrix_base T, sparse_matrix_base U>
+template<sparse_matrix_like T, sparse_matrix_like U> requires has_common_type<typename T::ValueType, typename U::ValueType>
 SparseMatrixCompareExpr<T, U> operator==(const T& a, const U& b) {
     return SparseMatrixCompareExpr<T, U>(Precision(epsilon<underlying_type_t<std::common_type_t<typename T::ValueType, typename U::ValueType>>>()), a, b);
 }
