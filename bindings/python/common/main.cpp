11d47b7d98c6eb62db7d29b583fe9a54c5dc3c9e
Ricardo Baucells <ricardo@baucells.net>
2026-08-18 16:39:47 -0400

    change order of precision to compare to fix issues with stubbs generation

--- bindings/python/common/main.cpp
+++ bindings/python/common/main.cpp
@@ -2,12 +2,12 @@
 
 #include <pybind11/pybind11.h>
 
-void common_compare_bindings(pybind11::module_& m);
 void common_precision_bindings(pybind11::module_& m);
+void common_compare_bindings(pybind11::module_& m);
 void common_rotation_bindings(pybind11::module_& m);
 
 void common_bindings(py::module_& m) {
-    common_compare_bindings(m);
     common_precision_bindings(m);
+    common_compare_bindings(m);
     common_rotation_bindings(m);
 }
