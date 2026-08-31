7796a7fb9dd85e00bb241a55d233e40e06cb6859
Ricardo Baucells <ricardo@baucells.net>
2026-08-30 20:41:43 -0400

    fix when elements.begin() doesnt have operator-> like in views transforms

--- include/mathpp/implementation/matrix/dense/matrix.h
+++ include/mathpp/implementation/matrix/dense/matrix.h
@@ -82,7 +82,7 @@ struct DenseMatrix {
     }
 
     template<std::ranges::sized_range R> requires std::ranges::sized_range<std::ranges::range_value_t<R>> && lossless_convertible<std::ranges::range_value_t<std::ranges::range_value_t<R>>, T>
-    DenseMatrix(const R& elements) : rows_(elements.size()), columns_(elements.begin()->size()), data_(new T[rows_ * columns_]) {
+    DenseMatrix(const R& elements) : rows_(elements.size()), columns_((*elements.begin()).size()), data_(new T[rows_ * columns_]) {
         std::size_t r = 0;
         for (const auto& row: elements) {
             if (row.size() != columns_) {
